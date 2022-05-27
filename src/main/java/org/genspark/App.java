276cc640edcda25678bab7aa2779cf2741edfcbe
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-05-27 15:28:10 -0400

    Initial commit

---
+++ src/main/java/org/genspark/App.java
@@ -0,0 +1,27 @@
+package org.genspark;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+
+        Student sdt = (Student) context.getBean("Student");
+
+        System.out.println(sdt.getName());
+        System.out.println(sdt.getId());
+        List<Phone> ph = sdt.getPh();
+        System.out.println(ph.get(0).getMob());
+        Address add = sdt.getAdd();
+        System.out.println(add.getCity());
+        System.out.println(add.getState());
+        System.out.println(add.getCountry());
+        System.out.println(add.getZipcode());
+    }
+
+}
---
+++ src/main/java/org/genspark/Phone.java
@@ -0,0 +1,13 @@
+package org.genspark;
+
+public class Phone {
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    private String mob;
+}
